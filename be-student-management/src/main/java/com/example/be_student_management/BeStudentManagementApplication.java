2d9522b3bf54a831189fa7f7e9139e145838970c
Your Nametrantiendat12345 <you@example.comtrantiendatas1@gmail.com>
2025-08-08 22:22:10 +0700

    create database

---
+++ be-student-management/src/main/java/com/example/be_student_management/BeStudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_student_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeStudentManagementApplication.class, args);
+	}
+
+}
